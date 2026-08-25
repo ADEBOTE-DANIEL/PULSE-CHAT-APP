30c527253926e1a538f29caeb4f80a13d073fb83
ADEBOTE-DANIEL <dannyadebote16@gmail.com>
2026-08-26 00:50:59 +0100

    Fix Django ASGI initialization order

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,24 +1,26 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.chat.jwt_auth_middleware import JWTAuthMiddleware
-from channels.security.websocket import AllowedHostsOriginValidator
-import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+import django
 django.setup()
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from apps.chat.jwt_auth_middleware import JWTAuthMiddleware
 from apps.chat.routing import websocket_urlpatterns
+
 
 asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': asgi_application,
+
     'websocket': AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(
-                websocket_urlpatterns
-            )
+            URLRouter(websocket_urlpatterns)
         )
     ),
 })
